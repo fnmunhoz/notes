7f42450828d845fdb3c0a3317f77b814066411dd
Felipe Munhoz <felipe@felipemunhoz.com>
2021-08-22 18:19:51 -0300

    feat: add dark mode (#12)
    If unsure about the inline script, see: https://www.joshwcomeau.com/react/dark-mode/

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,10 @@
-import Document, { DocumentContext } from 'next/document'
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -26,5 +32,31 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal()
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body data-theme="light">
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function () {
+                  try {
+                    var currentTheme = localStorage.getItem('theme');
+                    var prefersDarkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches === true;
+                    if (!currentTheme && prefersDarkTheme) document.body.dataset.theme = 'dark';
+                    if (!currentTheme) return;
+                    document.body.dataset.theme = currentTheme;
+                  } catch (e) {}
+                })();`,
+            }}
+          />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
